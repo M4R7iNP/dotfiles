4378e3a894de9d17df2bba0304fe0225f3221eb5
Martin Pedersen <martin@m4r7.in>
2021-07-21 16:09:43 +0200

    fix plug check

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -85,7 +85,7 @@ then
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
 fi
 
-if [ ! -e ${XDG_DATA_HOME:-$HOME/.local/share} ];
+if [ ! -e "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ];
 then
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
